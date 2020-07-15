99b58f9ec096e006b1c7b07197275b935bfd588c
vimal Raveendran <vimalraveendra@gmail.com>
2020-07-15 22:08:37 +0200

    Added TrelloCard component & styles

---
+++ src/Components/TrelloCard/TrelloCard.js
@@ -0,0 +1,18 @@
+import React from "react";
+import styles from "./TrelloCard.module.css";
+
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import Typography from "@material-ui/core/Typography";
+
+const TrelloCard = ({ text }) => {
+  return (
+    <Card className={styles.cardContainer}>
+      <CardContent>
+        <Typography>{text}</Typography>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default TrelloCard;
--- src/Container/App.js
+++ src/Container/App.js
@@ -3,15 +3,29 @@ import "./App.css";
 
 import TrelloLists from "../Components/TrelloList/TrelloList";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h2>Trello Board</h2>
-        <TrelloLists title={"Trello List"} />
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    cards: [
+      {
+        id: 0,
+        text: "We created a static list & static card",
+      },
+      {
+        id: 1,
+        text: "We created the second card",
+      },
+    ],
+  };
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h2>Trello Board</h2>
+          <TrelloLists title={"Trello List"} cardList={this.state.cards} />
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
