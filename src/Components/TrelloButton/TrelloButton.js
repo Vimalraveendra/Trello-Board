6578ea290583456ea02c7e60d77a259566829c55
vimal Raveendran <vimalraveendra@gmail.com>
2020-07-16 21:38:48 +0200

    Added TrelloButton Component & updated the TrelloList component with TrelloButton

---
+++ src/Components/TrelloButton/TrelloButton.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class TrelloButton extends Component {
+  render() {
+    return (
+      <div>
+        <h3>Trello Button</h3>
+      </div>
+    );
+  }
+}
+
+export default TrelloButton;
