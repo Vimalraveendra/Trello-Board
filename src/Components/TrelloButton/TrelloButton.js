88e95766e23917147237cfd5d03fbe2ad45a5aa4
vimal Raveendran <vimalraveendra@gmail.com>
2020-07-16 23:11:42 +0200

    Created TrelloButton styles

--- src/Components/TrelloButton/TrelloButton.js
+++ src/Components/TrelloButton/TrelloButton.js
@@ -1,12 +1,25 @@
 import React, { Component } from "react";
+import { Icon } from "@material-ui/core";
+
+import styles from "./TrelloButton.module.css";
 
 class TrelloButton extends Component {
-  render() {
+  // here we conditionally rendering the AddButton
+  renderAddButton = () => {
+    const { list } = this.props;
+    const buttonText = list ? ` Add another list` : `Add another card`;
     return (
-      <div>
-        <h3>Trello Button</h3>
+      <div
+        className={`${styles.buttonContainer}
+          {list ? styles.list : styles.cardList}`}
+      >
+        <Icon>add</Icon>
+        <p>{buttonText}</p>
       </div>
     );
+  };
+  render() {
+    return <div>{this.renderAddButton()}</div>;
   }
 }
 
