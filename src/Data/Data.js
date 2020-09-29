65c727410a57176a37ae80c64279940ea913a8e9
vimal Raveendran <vimalraveendra@gmail.com>
2020-09-29 19:21:03 +0200

    Created Data Folder & added CardList inside the Data file & also updated App component

---
+++ src/Data/Data.js
@@ -0,0 +1,54 @@
+const Data = [
+  {
+    title: "To Do List ",
+    id: 0,
+    cards: [
+      {
+        id: 0,
+        text: "We created a static list & static card",
+      },
+      {
+        id: 1,
+        text: "We created the second card",
+      },
+    ],
+  },
+  {
+    title: "Doing",
+    id: 1,
+    cards: [
+      {
+        id: 2,
+        text: "We created a static list & static card",
+      },
+      {
+        id: 3,
+        text: "We created the second card",
+      },
+      {
+        id: 4,
+        text: "We created a static list & static card",
+      },
+      {
+        id: 5,
+        text: "We created the second card",
+      },
+    ],
+  },
+  {
+    title: "Done",
+    id: 2,
+    cards: [
+      {
+        id: 6,
+        text: "We created a static list & static card",
+      },
+      {
+        id: 7,
+        text: "We created the second card",
+      },
+    ],
+  },
+];
+
+export default Data;
