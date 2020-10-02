facc48da4a40f4533fece82e99a8169699cfe18c
vimal Raveendran <vimalraveendra@gmail.com>
2020-10-02 18:01:08 +0200

    updated TrelloCard component to edit card items & also updated the TrelloForm component

--- src/Components/TrelloButton/TrelloButton.styles.jsx
+++ src/Components/TrelloButton/TrelloButton.styles.jsx
@@ -1,27 +1,30 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
+
+const ListStyle = css`
+  color: #fff;
+  opacity: 1;
+  background-color: rgba(0, 0, 0, 0.13);
+`;
+
+const CardStyle = css`
+  color: inherit;
+  opacity: 0.5;
+  background-color: inherit;
+`;
+
+const getCardStyles = (props) => {
+  return props.list ? ListStyle : CardStyle;
+};
 
 export const ButtonContainer = styled.div`
-    display: flex;
-    align-items: center;
-    cursor: pointer;
-    height: 10px;
-    width: 250px;
-    padding: 20px;
-    &:hover {
+  display: flex;
+  align-items: center;
+  cursor: pointer;
+  height: 10px;
+  width: 250px;
+  padding: 20px;
+  &:hover {
     background-color: rgba(0, 0, 0, 0.13);
   }
-   `;
-
-  
-  .list {
-    color: #fff;
-    opacity: 1;
-    background-color: rgba(0, 0, 0, 0.13);
-  }
-  
-  .cardList {
-    color: inherit;
-    opacity: 0.5;
-    background-color: inherit;
-  }
-  
+  ${getCardStyles}
+`;
